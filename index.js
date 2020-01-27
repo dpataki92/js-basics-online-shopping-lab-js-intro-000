632992366a632fa9053c34763bee46d4ce9fe263
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-01-27 15:45:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
-function getCart() {
- return cart;
+function getCart(name) {
+  return cart;
 }
 
 function setCart(c) {
@@ -11,18 +11,44 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+  let price = Math.floor(Math.random()*100);
+
+  cart.push({ `itemName:${name}, itemPrice: ${price}` }})
+ 
+  return `${name} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {return 'Your shopping cart is empty.'}
+
+  let str = `In your cart, you have `;
+
+  for (let i = 0; i < cart.length; i++) {
+    i !== cart.length-1 ? str += `${cart[i]['itemName']} at $${cart[i]['itemPrice']}, ` : `and ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`
+  }
+
+  return str;
 }
 
 function total() {
   // write your code here
+  let count = 0;
+
+  for (let i = 0; i < cart.length; i++) {
+    count += cart[i]['itemPrice'];
+  }
+
+  return count;
 }
 
 function removeFromCart(item) {
   // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (item === cart[i]['itemName']) {cart.splice(i, 1)}
+  }
+
+  return 'That item is not in your cart.';
 }
 
 function placeOrder(cardNumber) {
